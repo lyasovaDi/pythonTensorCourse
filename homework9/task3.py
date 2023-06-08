421e8ccb3d9ee218eeb5a4cb529417447452953c
lyasovaDi <di.ilyasova@tensor.ru>
2023-06-08 20:54:41 +0300

    update homework9

---
+++ homework9/task3.py
@@ -0,0 +1,22 @@
+# Дан файл test_file/task_3.txt можно считать, что это запись покупок в магазине, где указана только цена товара
+# В каждой строке файла записана цена товара.
+# Покупки (т.е. несколько подряд идущих цен) разделены пустой строкой
+# Нужно найти сумму трёх самых дорогих покупок, которые запишутся в переменную three_most_expensive_purchases
+
+# Здесь пишем код
+
+file = open('test_file/task_3.txt', 'r')
+mass1 = file.readlines()
+mass2 = []
+summ = 0
+for line in mass1:
+    if line != "\n":
+        summ += int(line)
+    else:
+        mass2.append(summ)
+        summ = 0
+
+sort_mass = sorted(mass2)
+three_most_expensive_purchases = sort_mass[-1] + sort_mass[-2] + sort_mass[-3]
+
+assert three_most_expensive_purchases == 202346
